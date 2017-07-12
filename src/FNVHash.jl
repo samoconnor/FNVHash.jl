ab793d8b3ced26a449f5ed2893013a7c468532d0
Sam O'Connor <sam@octech.com.au>
2017-07-12 11:52:58 +1000

    Julia 0.6 compat

--- src/FNVHash.jl
+++ src/FNVHash.jl
@@ -5,20 +5,22 @@ module FNVHash
 
 export fnv32, fnv16
 
+if VERSION < v"0.6.0" const xor = $ end # FIXME
+
 # References:
 #   http://www.isthe.com/chongo/tech/comp/fnv/
 #   https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
 #   http://tools.ietf.org/html/draft-eastlake-fnv-03
 
-function fnv32(data::Array{UInt8,1})
+function fnv32(data::Vector{UInt8})
     hash = 0x811C9DC5
     for c in data
-        hash = (hash * 0x01000193) $ c
+        hash = xor((hash * 0x01000193), c)
     end
     return hash
 end
 
-fnv32(s::AbstractString) = fnv32(Array{UInt8,1}(s))
+fnv32(s) = fnv32(Vector{UInt8}(s))
 
 
 fnv16(hash::UInt32) = UInt16(hash >>> 16) $ UInt16(hash & 0xFFFF)
