8d9d467fd23cf52edd18774c9541139ecc95b1ce
Christian Kurz <chrissikurz@gmail.com>
2018-04-24 10:05:22 +0200

    Use FNV1a instead

--- src/FNVHash.jl
+++ src/FNVHash.jl
@@ -8,14 +8,14 @@ export fnv32, fnv16
 if VERSION < v"0.6.0" const xor = $ end # FIXME
 
 # References:
-#   http://www.isthe.com/chongo/tech/comp/fnv/
+#   http://www.isthe.com/chongo/tech/comp/fnv/#FNV-1a
 #   https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
 #   http://tools.ietf.org/html/draft-eastlake-fnv-03
 
 function fnv32(data::Vector{UInt8})
     hash = 0x811C9DC5
     for c in data
-        hash = xor((hash * 0x01000193), c)
+        hash = xor(hash, c) * 0x01000193
     end
     return hash
 end
@@ -23,7 +23,7 @@ end
 fnv32(s) = fnv32(Vector{UInt8}(s))
 
 
-fnv16(hash::UInt32) = UInt16(hash >>> 16) $ UInt16(hash & 0xFFFF)
+fnv16(hash::UInt32) = UInt16(hash >>> 16) $ UInt16(hash) & 0xFFFF
 fnv16(data) = fnv16(fnv32(data))
 
 
