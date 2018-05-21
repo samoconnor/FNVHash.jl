37cbc4b458cde0b13e383321a8acbd1499682c82
Christian Kurz <chrissikurz@gmail.com>
2018-05-21 13:07:22 +0200

    Add FNV1a instead of replacing FNV

--- src/FNVHash.jl
+++ src/FNVHash.jl
@@ -8,14 +8,15 @@ export fnv32, fnv16
 if VERSION < v"0.6.0" const xor = $ end # FIXME
 
 # References:
-#   http://www.isthe.com/chongo/tech/comp/fnv/#FNV-1a
 #   https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
 #   http://tools.ietf.org/html/draft-eastlake-fnv-03
 
+# FNV1
+#   http://www.isthe.com/chongo/tech/comp/fnv/#FNV-1
 function fnv32(data::Vector{UInt8})
     hash = 0x811C9DC5
     for c in data
-        hash = xor(hash, c) * 0x01000193
+        hash = xor((hash * 0x01000193), c)
     end
     return hash
 end
@@ -25,6 +26,22 @@ fnv32(s) = fnv32(Vector{UInt8}(s))
 
 fnv16(hash::UInt32) = UInt16(hash >>> 16) $ UInt16(hash) & 0xFFFF
 fnv16(data) = fnv16(fnv32(data))
+
+
+# FNV1a
+#   http://www.isthe.com/chongo/tech/comp/fnv/#FNV-1a
+function fnv1a32(data::Vector{UInt8})
+    hash = 0x811C9DC5
+    for c in data
+        hash = xor(hash, c) * 0x01000193
+    end
+    return hash
+end
+
+fnv1a32(s) = fnv1a32(Vector{UInt8}(s))
+
+fnv1a16(hash::UInt32) = UInt16(hash >>> 16) $ UInt16(hash) & 0xFFFF
+fnv1a16(data) = fnv1a16(fnv1a32(data))
 
 
 end # module FNVHash
